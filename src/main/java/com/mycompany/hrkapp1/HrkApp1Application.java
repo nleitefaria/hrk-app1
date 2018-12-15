41be9fe78a954396add29d2879f37c2692fd502d
nleitefaria <nleitefaria@gmail.com>
2018-12-15 17:23:31 +0000

    INIT

---
+++ src/main/java/com/mycompany/hrkapp1/HrkApp1Application.java
@@ -0,0 +1,14 @@
+package com.mycompany.hrkapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrkApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrkApp1Application.class, args);
+	}
+
+}
+
