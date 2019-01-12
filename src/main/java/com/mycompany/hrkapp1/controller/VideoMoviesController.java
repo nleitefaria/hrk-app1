357d19822983f6366e6b9d96f0248d84c5d96751
nleitefaria <nleitefaria@gmail.com>
2019-01-12 22:52:42 +0000

    Added findOne to VideoMovies

--- src/main/java/com/mycompany/hrkapp1/controller/VideoMoviesController.java
+++ src/main/java/com/mycompany/hrkapp1/controller/VideoMoviesController.java
@@ -1,8 +1,11 @@
 package com.mycompany.hrkapp1.controller;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mycompany.hrkapp1.entity.VideoMovies;
@@ -12,6 +15,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api")
@@ -28,9 +32,27 @@ public class VideoMoviesController
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
     }
     ) 
-	@RequestMapping(value = "/video/movies", method = RequestMethod.GET)
-	public Flux<VideoMovies> findAll() 
+	@GetMapping(value = "/video/movies")
+	public Flux<ResponseEntity<VideoMovies>> findAll() 
 	{
-	  return repository.findAll();
+	  return repository.findAll()
+			  .map(videoMovies -> ResponseEntity.ok(videoMovies))
+              .defaultIfEmpty(ResponseEntity.notFound().build());
 	}
+	
+	@ApiOperation(value = "Get movie by id",response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    ) 
+	@GetMapping("/video/movie/{id}")
+    public Mono<ResponseEntity<VideoMovies>> getTweetById(@PathVariable(value = "id") String id) 
+	{
+        return repository.findById(new ObjectId(id))
+                .map(videoMovie -> ResponseEntity.ok(videoMovie))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
 }
